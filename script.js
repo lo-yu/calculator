b0102a3cd8204e7c517b2c6bebddb858da374170
Lobsang Yungdung <[10lobsangyungdung10@gmail.com]>
2025-05-10 12:56:04 +0530

    Create operate to perform one of the operation

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+let no1, no2, operator;
+
 let add = function( a, b){
     return a + b;
-
 }
 
 let sub = function(a,b){
-    return a - b;
-    
+    return a - b;   
 }
 
 let multiply = function(a,b){
-    return a * b;
-    
+    return a * b;   
 }
 
 let divide = function(a,b){
     return a / b;
-    
+}
+
+let operate = function(n1, op, n2){
+    if (op == "+"){
+        add(n1, n2);
+
+    }
+    else if (op == "-"){
+        sub(n1, n2);
+        
+    }
+    else if (op == "*"){
+        multiply(n1, n2);
+        
+    }
+    else if (op == "/"){
+        divide(n1, n2);
+        
+    }
+
 }
