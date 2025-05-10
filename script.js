a5efe560be255b2efd282a72b8724aa85b0a0266
Lobsang Yungdung <[10lobsangyungdung10@gmail.com]>
2025-05-10 12:40:23 +0530

    Create the basic math function

---
+++ script.js
@@ -0,0 +1,19 @@
+let add = function( a, b){
+    return a + b;
+
+}
+
+let sub = function(a,b){
+    return a - b;
+    
+}
+
+let multiply = function(a,b){
+    return a * b;
+    
+}
+
+let divide = function(a,b){
+    return a / b;
+    
+}
